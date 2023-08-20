9f73167291e5743fd60888ffc991363fca0cc8a5
kimkyumbi <kimgyeombi@naver.com>
2023-08-20 14:39:41 +0900

    BeerList

--- beerlist with B/View/BeerListView.swift
+++ beerlist with B/View/BeerListView.swift
@@ -13,20 +13,7 @@ struct BeerListView: View {
             List {
                 HStack {
                     NavigationLink("Vice Bier") {
-                        Image("keg")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 300, height: 300)
-                        Text("21")
-                            .font(.system(size: 12))
-                            .foregroundColor(Color.yellow)
-                        Text("Vice Bier")
-                            .font(.system(size: 30))
-                        Text("Our take on the classic German Kristallweizen. A clear German wheat beer, layers of bubblegum and vanilla perfectly balanced with the American and New Zealand hops.")
-                            .font(.system(size: 22))
-                            .padding(.horizontal)
-                            .foregroundColor(Color.gray)
-                        Spacer()
+                        BeerListDetailView()
                     }
                 }
             }
