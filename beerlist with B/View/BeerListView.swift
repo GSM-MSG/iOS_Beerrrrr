57e0ea62d98bdd3c15b5b73a6a2b12334f8a6513
kimkyumbi <kimgyeombi@naver.com>
2023-09-16 15:00:59 +0900

    BeerList

--- beerlist with B/View/BeerListView.swift
+++ beerlist with B/View/BeerListView.swift
@@ -6,30 +6,62 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct BeerListView: View {
+    let beerListAPI = BeerListAPI()
+    @State private var beerlist: BeerList?
+    
     var body: some View {
         NavigationStack {
             List {
                 HStack {
-                    Image("keg")
-                        .resizable()
-                        .frame(width: 63, height: 125)
-                        .padding(.trailing, 90)
-                    NavigationLink(destination: BeerListDetailView(), label: {
-                        Text("Vice Bier")
-                    })
-                    .font(.system(size: 30))
+                    if let beerlist {
+                        KFImage(URL(string: beerlist.imageURL))
+                            .resizable()
+                            .frame(width: 63, height: 125)
+                            .padding(.trailing, 90)
+                        NavigationLink(destination: BeerListDetailView(), label: {Text(beerlist.name)})
+                        .font(.system(size: 30))
+                    } else {
+                        ProgressView()
+                    }
                 }
             }
             .navigationTitle("Beer List")
             .listStyle(.plain)
         }
+        .onAppear{
+            BeerListAPI.getBeerListData { beerlist in
+                print(beerlist)
+                self.beerlist = beerlist
+            }
+        }
     }
 }
-
 struct BeerListView_Previews: PreviewProvider {
     static var previews: some View {
         BeerListView()
     }
 }
+
+//if let beer {
+//                    KFImage(URL(string:beer.imageURL))
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fit)
+//                        .frame(width: 300, height: 300)
+//
+//                    Text("\(beer.beerId)")
+//                        .font(.system(size: 12))
+//                        .foregroundColor(Color.orange)
+//
+//                    Text(beer.name)
+//                        .font(.system(size: 30))
+//
+//                    Text(beer.description)
+//                        .font(.system(size: 22))
+//                        .padding(.horizontal)
+//                        .foregroundColor(Color.gray)
+//                } else {
+//                    ProgressView()
+//                }
